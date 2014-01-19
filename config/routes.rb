6a775c97c29a0b416720546e646e7030751df903
vimcmd <vim.cmd@gmail.com>
2014-01-19 21:34:29 +0600

    Styling part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Icrb::Application.routes.draw do
-  get "static_pages/home"
+  root              to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help'
 
-  get "static_pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
